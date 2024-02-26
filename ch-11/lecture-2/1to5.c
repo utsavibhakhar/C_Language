046033d9cefdcbef48ed06e3be9fb9077b816108
utsavibhakhar <158564889+utsavibhakhar@users.noreply.github.com>
2024-02-26 19:44:54 +0530

    Add files via upload

---
+++ ch-11/lecture-2/1to5.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main()
+{
+	int i,j,s;
+	
+	for(i=1;i<=5;i++)
+	{
+		for(s=4;s>=i;s--)
+		{
+			printf(" ");
+		}
+			for(j=i;j>=1;j--)
+			{
+				printf("%d",j);
+			}
+		printf("\n");
+	}
+	
+}
